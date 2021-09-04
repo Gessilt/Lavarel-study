967a0a532d473cab0d96762ec19ee4776a879708
gessilt <henriquefreitas2710@gmail.com>
2021-09-04 10:18:57 -0300

    Introduction to laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Blade deixa as rotas dinâmicas, é a template engine do laravel: Pode-se digitar com HTML puro e pegar os dados fornecidos pelo controller
+// Views são renderizadas pelo blade.
+
+
+// Nomear as rotas sempre com "".blade.php
+// O nome que importa (vai chamar a página) é o anterior a function, no view(), pode inserir qualquer nome, pois este será o da URL.
+// Posso ter uma URL diferente, mas chamar uma view anterior, como a welcome, acima.
+
+Route::get('/new', function () {
+
+    $name = "Matheus";
+
+    $names = ["Júlio","Levi","Ronaldo","Battisti"];
+
+    $age = 23;
+
+    $numbers = [1,2,3,4,5];
+
+    return view('new',['name' => $name, 'age' => $age, "numbers" => $numbers, "names" => $names]);
+});
